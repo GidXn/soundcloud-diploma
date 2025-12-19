7cf743fc1f70a3e861cd1aa6d1c112be243ea2f5
GidXn <michael.gydin@gmail.com>
2025-12-19 18:49:15 +0200

    Add EF Core with PostgreSQL and Genre entity support
    Integrated Entity Framework Core with PostgreSQL provider.
    Added SoundCloudDbContext and GenreEntity for genres table.
    Configured connection string and DI in startup.
    Included initial migration to create Genres table.

---
+++ soundcloud-back/Data/Entities/GenreEntity.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace soundcloud_back.Data.Entities
+{
+    public class GenreEntity
+    {
+        [Key]
+        public int Id { get; set; } // Відповідає genre_id в базі даних
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; } // Назва жанру
+        public int PlayCount { get; set; } = 0;
+
+    }
+}
--- soundcloud-back/Program.cs
+++ soundcloud-back/Program.cs
@@ -1,6 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using soundcloud_back.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<SoundCloudDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 
