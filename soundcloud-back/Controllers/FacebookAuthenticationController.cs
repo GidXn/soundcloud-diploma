ae2618a6bd4bfc19a69d643e1b456077414a53b9
KrukRoma <roma.0428.3675@gmail.com>
2026-02-06 20:24:20 +0200

    Add Swagger fix

--- soundcloud-back/Controllers/FacebookAuthenticationController.cs
+++ soundcloud-back/Controllers/FacebookAuthenticationController.cs
@@ -4,67 +4,67 @@ using soundcloud_back.Services.Interfaces;
 
 namespace soundcloud_back.Controllers;
 
-[ApiController]
-[Route("auth")]
-public class FacebookAuthenticationController : ControllerBase
-{
-    private readonly IFacebookTokenValidator _facebook;
-    private readonly IUserService _users;
-    private readonly IAuthService _auth;
+//[ApiController]
+//[Route("auth")]
+//public class FacebookAuthenticationController : ControllerBase
+//{
+//    private readonly IFacebookTokenValidator _facebook;
+//    private readonly IUserService _users;
+//    private readonly IAuthService _auth;
 
-    public FacebookAuthenticationController(
-        IFacebookTokenValidator facebook,
-        IUserService users,
-        IAuthService auth)
-    {
-        _facebook = facebook;
-        _users = users;
-        _auth = auth;
-    }
+//    public FacebookAuthenticationController(
+//        IFacebookTokenValidator facebook,
+//        IUserService users,
+//        IAuthService auth)
+//    {
+//        _facebook = facebook;
+//        _users = users;
+//        _auth = auth;
+//    }
 
-    public class TokenRequest { public string Token { get; set; } = ""; }
+//    public class TokenRequest { public string Token { get; set; } = ""; }
 
-    [AllowAnonymous]
-    [HttpPost("facebook")]
-    public async Task<IActionResult> FacebookLogin([FromBody] TokenRequest req)
-    {
-        if (string.IsNullOrWhiteSpace(req?.Token))
-            return BadRequest(new { error = "Token is required" });
+//    [AllowAnonymous]
+//    [HttpPost("facebook")]
+//    public async Task<IActionResult> FacebookLogin([FromBody] TokenRequest req)
+//    {
+//        if (string.IsNullOrWhiteSpace(req?.Token))
+//            return BadRequest(new { error = "Token is required" });
 
-        try
-        {
-            var userInfo = await _facebook.ValidateAsync(req.Token);
+//        try
+//        {
+//            var userInfo = await _facebook.ValidateAsync(req.Token);
 
-            if (string.IsNullOrWhiteSpace(userInfo.Email))
-                return BadRequest(new { error = "Email is required from Facebook account" });
+//            if (string.IsNullOrWhiteSpace(userInfo.Email))
+//                return BadRequest(new { error = "Email is required from Facebook account" });
 
-            var user = await _users.FindOrCreateFromFacebookAsync(
-                userInfo.Id,
-                userInfo.Email,
-                userInfo.Name,
-                userInfo.Picture);
+//            var user = await _users.FindOrCreateFromFacebookAsync(
+//                userInfo.Id,
+//                userInfo.Email,
+//                userInfo.Name,
+//                userInfo.Picture);
 
-            var jwt = _auth.IssueJwtForUser(user);
+//            var jwt = _auth.IssueJwtForUser(user);
 
-            return Ok(new
-            {
-                token = jwt,
-                expiresAt = DateTime.UtcNow.AddHours(1),
-                id = user.Id,
-                username = user.Username,
-                email = user.Email,
-                avatarUrl = user.AvatarUrl
-            });
-        }
-        catch (UnauthorizedAccessException ex)
-        {
-            Console.WriteLine("[AUTH/FACEBOOK] UnauthorizedAccessException: " + ex);
-            return BadRequest(new { error = ex.Message });
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine("[AUTH/FACEBOOK] ERROR: " + ex);
-            return StatusCode(500, new { error = ex.Message });
-        }
-    }
-}
+//            return Ok(new
+//            {
+//                token = jwt,
+//                expiresAt = DateTime.UtcNow.AddHours(1),
+//                id = user.Id,
+//                username = user.Username,
+//                email = user.Email,
+//                avatarUrl = user.AvatarUrl
+//            });
+//        }
+//        catch (UnauthorizedAccessException ex)
+//        {
+//            Console.WriteLine("[AUTH/FACEBOOK] UnauthorizedAccessException: " + ex);
+//            return BadRequest(new { error = ex.Message });
+//        }
+//        catch (Exception ex)
+//        {
+//            Console.WriteLine("[AUTH/FACEBOOK] ERROR: " + ex);
+//            return StatusCode(500, new { error = ex.Message });
+//        }
+//    }
+//}
