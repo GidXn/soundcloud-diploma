d3edbc81d7f545cb64f0e9d8b1d226e8cf31f5af
GidXn <michael.gydin@gmail.com>
2026-02-11 20:29:54 +0200

    albums models

---
+++ soundcloud-back/Models/Album/AlbumSummaryDto.cs
@@ -0,0 +1,10 @@
+﻿namespace soundcloud_back.Models.Album
+{
+    public class AlbumSummaryDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = "";
+        public int ArtistId { get; set; } = 0;
+        public string? ImageUrl { get; set; }
+    }
+}
---
+++ soundcloud-back/Models/Album/CreateAlbumDto.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
+
+namespace soundcloud_back.Models.Album
+{
+    public class CreateAlbumDto
+    {
+        [Required]
+        [MaxLength(200)]
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        // Обов'язковий власник альбому
+        [Required]
+        public int OwnerId { get; set; }
+
+        public IFormFile? CoverUrl { get; set; }
+
+        // Нове поле: публічність альбому
+        public bool IsPublic { get; set; } = true; // за замовчуванням публічний
+    }
+}
