4dc0e7bdfbd552a31e224e51ab0661e10d819897
GidXn <michael.gydin@gmail.com>
2026-02-11 20:30:47 +0200

    genre models

---
+++ soundcloud-back/Models/Genre/GenreUpdateDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace soundcloud_back.Models.Genre
+{
+    public class GenreUpdateDto
+    {
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+    }
+}
