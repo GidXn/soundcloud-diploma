961888f17e46ca03b8b3915185824966c8fec0bf
GidXn <michael.gydin@gmail.com>
2026-02-11 20:35:48 +0200

    search model

---
+++ soundcloud-back/Models/PagedResult.cs
@@ -0,0 +1,10 @@
+﻿namespace soundcloud_back.Models
+{
+    public class PagedResult<T>
+    {
+        public IEnumerable<T> Items { get; set; } = Enumerable.Empty<T>();
+        public int Page { get; set; }
+        public int PageSize { get; set; }
+        public int Total { get; set; }
+    }
+}
---
+++ soundcloud-back/Models/SearchResponseDto.cs
@@ -0,0 +1,15 @@
+﻿using soundcloud_back.Models.Album;
+using soundcloud_back.Models.Auth;
+using soundcloud_back.Models.Playlist;
+using soundcloud_back.Models.Track;
+
+namespace soundcloud_back.Models
+{
+    public class SearchResponseDto
+    {
+        public PagedResult<TrackSummaryDto> Tracks { get; set; } = new();
+        public PagedResult<AlbumSummaryDto> Albums { get; set; } = new();
+        public PagedResult<PlaylistSummaryDto> Playlists { get; set; } = new();
+        public PagedResult<UserSummaryDto> Users { get; set; } = new();
+    }
+}
