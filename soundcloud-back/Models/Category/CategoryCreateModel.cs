afed6e50a6d522317e4599bf5d2b47f2fba9baa6
GidXn <michael.gydin@gmail.com>
2026-02-11 20:28:18 +0200

    category models

---
+++ soundcloud-back/Models/Category/CategoryCreateModel.cs
@@ -0,0 +1,9 @@
+﻿namespace soundcloud_back.Models.Category;
+
+public class CategoryCreateModel
+{
+    public string Name { get; set; } = string.Empty;
+    public string Slug { get; set; } = string.Empty;
+    public int PlayCount { get; set; } = 0;
+    public IFormFile? ImageFile { get; set; } = null;
+}
---
+++ soundcloud-back/Models/Category/CategoryUpdateModel.cs
@@ -0,0 +1,10 @@
+﻿namespace soundcloud_back.Models.Category
+{
+    public class CategoryUpdateModel
+    {
+        public string Name { get; set; } = "";
+        public string Slug { get; set; } = "";
+        public int PlayCount { get; set; } = 0;
+        public IFormFile? ImageFile { get; set; }
+    }
+}
